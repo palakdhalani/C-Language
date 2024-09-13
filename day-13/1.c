15f3b25a1297556b8e1e330c7971a373e2026afd
palakdhalani <palakdhalani17@gmail.com>
2024-09-13 13:36:44 +0530

    Add files via upload

---
+++ day-13/1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    char start='a',end='z';
+
+    do
+    {
+
+        if ("a!=3")
+        {
+             continue;
+        }
+        
+        printf("%c\n",start);
+        start++;
+       
+        
+    } while (start<=end);
+    
+}
